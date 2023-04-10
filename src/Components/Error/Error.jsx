523c14a15d8430943fdc7faef3eab71a4999d404
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-04-10 11:11:17 +0600

    error 404 page add some style

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -7,16 +7,16 @@ const Error = () => {
     return (
         <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
             <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
-                <FaceFrownIcon className='w-40 h-40 text-yellow-500' />
+                <FaceFrownIcon className='w-40 h-40 text-rose-400' />
                 <div className='max-w-md text-center'>
-                    <h2 className='mb-8 font-extrabold text-9xl text-yellow-500'>
+                    <h2 className='mb-8 font-extrabold text-9xl text-red-600'>
                         <span className='sr-only'>Error</span>
                         {status || 404}
                     </h2>
                     <p className='text-2xl font-semibold md:text-3xl text-red-800 mb-8'>
                         {error?.message}
                     </p>
-                    <Link to='/' className='btn'>
+                    <Link to='/' className='btn bg-[#9873FF] text-white p-3 rounded-md'>
                         Back to homepage
                     </Link>
                 </div>
