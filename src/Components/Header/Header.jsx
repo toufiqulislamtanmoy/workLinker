c0b996226642486d13afd90c3e59a4ffaeb44989
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-04-11 11:57:47 +0600

    fix the mobile navbar route

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -111,7 +111,7 @@ const Header = () => {
                                             </li>
                                             <li>
                                                 <Link
-                                                    to='/applyingjobs'
+                                                    to='/appliedjobs'
                                                     className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
                                                 >
                                                     Applied Jobs
